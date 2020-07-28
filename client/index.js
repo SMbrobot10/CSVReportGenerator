4c32d09a31c5916c7a174cf14cc6a39697193de4
Steven <stevenmbrotherton@gmail.com>
2020-07-28 11:04:57 -0500

    Start project

---
+++ client/index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset ='utf-8'/>
+    <title>CSV Report Generator</title>
+  </head>
+  <body>
+    <form>
+      <textarea id = 'csvInput' name = 'CSV INPUT' cols = '60' rows = '10'></textarea>
+      <input type='submit' value='SUBMIT'/>
+    </form>
+
+  </body>
+</html>
